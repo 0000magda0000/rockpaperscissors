fbf0a49956e1ab29e886cb24f1b42892e2f244cf
Magda Barthofer <magda.barthofer@gmail.com>
2020-03-09 19:12:11 +0100

    changed styles

--- rps-try.js
+++ rps-try.js
@@ -7,60 +7,61 @@ alert(`Hello ${pName}, let's play a game of rock, paper, scissors`)
 let cCount = 0;
 let pCount = 0;
 
+
 function computerPlay () {
 let options = ["rock", "paper", "scissors"];
 let computerChoice = options[Math.floor(Math.random() * options.length)];
 return computerChoice
 }
 
-
 let rockButton = document.querySelector("#rock");
 let paperButton = document.querySelector("#paper");
 let scissorsButton = document.querySelector("#scissors");
-
-
 rockButton.addEventListener("click", () => playRound("rock", computerPlay()));
 paperButton.addEventListener("click", () => playRound("paper", computerPlay()));
 scissorsButton.addEventListener("click", () => playRound("scissors", computerPlay()));
 
 
+
 function playRound(playerSelection, computerSelection) {
+
+  let div = document.querySelector("div")
 if (playerSelection == "rock" && computerSelection == "rock") {
-  alert(`It's a tie, you both chose ${computerSelection}. ${pName} ${cCount}: Computer ${pCount}`);
+  div.innerHTML = `It's a tie, you both chose ${computerSelection}. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "rock" && computerSelection == "paper") {
-  pCount+=1;
-  alert(`You loose, paper wins over ${playerSelection}. ${pName} ${cCount}: Computer ${pCount}`);
+  cCount+=1;
+  div.innerHTML = `You loose, paper wins over ${playerSelection}. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "rock" && computerSelection =="scissors") {
-  cCount +=1;
-  alert(`You win, ${computerSelection} get smashed. ${pName} ${cCount}: Computer ${pCount}`);
+  pCount +=1;
+  div.innerHTML = `You win, ${computerSelection} get smashed. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "paper" && computerSelection == "paper") {
-  alert(`It's a tie, you both chose paper. ${pName} ${cCount}: Computer ${pCount}`);
+  div.innerHTML = `It's a tie, you both chose paper. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "paper" && computerSelection == "rock") {
-  cCount=+1;
-  alert(`You win, ${computerSelection} looses. ${pName} ${cCount}: Computer ${pCount}`);
+  pCount=+1;
+  div.innerHTML = `You win, ${computerSelection} looses. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "paper" && computerSelection =="scissors") {
-  pCount+=1;
-  alert(`You loose, paper get cut. ${pName} ${cCount}: Computer ${pCount}`);
+  cCount+=1;
+  div.innerHTML = `You loose, paper get cut. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "scissors" && computerSelection == "scissors") {
-  alert(`It's a tie, you both chose ${computerSelection}. ${pName} ${cCount}: Computer ${pCount}`);
+  div.innerHTML = `It's a tie, you both chose ${computerSelection}. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "scissors" && computerSelection == "paper") {
-  cCount +=1;
-  alert(`You win, paper gets cut. ${pName} ${cCount}: Computer ${pCount}`);
+  pCount +=1;
+  div.innerHTML = `You win, paper gets cut. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }
 else if (playerSelection == "scissors" && computerSelection =="rock") {
-  pCount=+1;
-  alert(`You loose, ${playerSelection} get smashed. ${pName} ${cCount}: Computer ${pCount}`);
+  cCount=+1;
+  div.innerHTML = `You loose, ${playerSelection} get smashed. <br> ${pName} : Computer <br> ${pCount} : ${cCount}`;
 }}
 
 
-function result() {
+function result () {
 if (cCount < pCount) {
    alert(`Final Score: ${pCount}:${cCount} Congratulations! ${pName} is the winner!`);
 }
@@ -71,5 +72,4 @@ else if (cCount > pCount) {
 
 else if (cCount == pCount) {
   alert(`Final Score: ${pCount}:${cCount} This game is a draw`)
-}
-}
+}}
